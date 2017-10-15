7c337e91cd15e2f5b79d89654c2a09899d64e6de
Endre Márk Borza <endremborza@gmail.com>
2017-10-15 14:57:14 +0200

    second commit

---
+++ week5.py
@@ -0,0 +1,12 @@
+def search(v,k):
+    n = len(v)
+    for i in range(n):
+        if k == v[i]
+            return i
+    return "nincs"
+
+v = [1,4,7,3,"fing",False,123,"busz"]
+
+print search(v,"fing")
+
+print search(v,"poo")
