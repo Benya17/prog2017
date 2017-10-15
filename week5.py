839989b121dbe7a36b76618fd1f68d2ce9804cbe
Endre Márk Borza <endremborza@gmail.com>
2017-10-15 15:11:09 +0200

    4th commit

--- week5.py
+++ week5.py
@@ -5,9 +5,10 @@ def search(v,k):
             return i
     return "nincs"
 
-v = [1,4,7,3,"fing",False,123,"busz"]
+v = [1,4,7,3,"fing",False,123,"busz",7]
 
 print search(v,"fing")
 
 print search(v,"poo")
 
+print(search(v,7))
